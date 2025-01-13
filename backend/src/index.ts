e366bfca9989bc16402d1d1b75e25badbe396a11
Pravesh Rathore <prave@LAPTOP-M250P136>
2025-01-13 22:34:58 +0530

    Setup backend server with Express

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
